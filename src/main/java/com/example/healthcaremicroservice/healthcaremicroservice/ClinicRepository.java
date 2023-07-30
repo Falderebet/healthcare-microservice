31e350fcbdfe406b75ed591ce0d4937728d5612e
Frederik <frederik.nic@gmail.com>
2023-07-30 14:34:57 +0200

    Added clinicrepository

--- src/main/java/com/example/healthcaremicroservice/healthcaremicroservice/ClinicRepository.java
+++ src/main/java/com/example/healthcaremicroservice/healthcaremicroservice/ClinicRepository.java
@@ -1,4 +1,6 @@
 package com.example.healthcaremicroservice.healthcaremicroservice;
 
-public class ClinicRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClinicRepository extends JpaRepository<Clinic, Long> {
 }
