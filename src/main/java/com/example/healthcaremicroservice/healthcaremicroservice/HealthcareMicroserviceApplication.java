1a5a2e79b205c2f797d7fe87cc033cda86245773
Frederik <frederik.nic@gmail.com>
2023-07-30 14:05:00 +0200

    Initial commit

---
+++ src/main/java/com/example/healthcaremicroservice/healthcaremicroservice/HealthcareMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcaremicroservice.healthcaremicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthcaremicroservice/healthcaremicroservice/HealthcareMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcaremicroservice.healthcaremicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
